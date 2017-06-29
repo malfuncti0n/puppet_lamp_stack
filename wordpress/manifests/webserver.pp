68372342acb6e466909df44660751e4feb9da913
mafuncti0n <savvaniss@yahoo.gr>
2017-06-29 11:39:53 +0300

    create apache with default vhost, add some config values for mysql

---
+++ wordpress/manifests/webserver.pp
@@ -0,0 +1,18 @@
+class wordpress::web {
+
+    # Install Apache
+    class {'apache': 
+        mpm_module => 'prefork'
+
+    }
+
+	apache::vhost { "${wordpress::config::severname}" :
+	  port          => '80',
+	  docroot       => "/var/www/html/"${wordpress::config::severname}",
+	  docroot_owner => 'www-data',
+	  docroot_group => 'www-data',
+} 
+    # Add support for PHP 
+    class {'::apache::mod::php': }
+
+}
